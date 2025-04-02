e6893878524d3e6b4d6e0fbfb396b21b5628fc0f
flaviolunaferreira <flaviolunaferreira@gmail.com>
2025-04-01 21:28:30 -0300

    Escrevendo interface de serviços

---
+++ src/main/java/com/portfolio/service/ProjetoService/ProjetoService.java
@@ -0,0 +1,9 @@
+package com.portfolio.service.ProjetoService;
+
+import com.portfolio.dto.projeto.ProjetoResponseDTO;
+
+import java.util.List;
+
+public interface ProjetoService {
+    List<ProjetoResponseDTO> listarProjetos();
+}
---
+++ src/main/java/com/portfolio/service/ProjetoService/TarefaService.java
@@ -0,0 +1,4 @@
+package com.portfolio.service.ProjetoService;
+
+public interface TarefaService {
+}
